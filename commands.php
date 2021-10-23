675cfb1ae83720666ea42675c0cdd26a4af22014
Lalu Kismara Hadi <n.kismara@gmail.com>
2021-10-23 16:42:16 +0700

    add divide command
    add new feature to calculator, divide command

--- commands.php
+++ commands.php
@@ -3,5 +3,6 @@
 return [
     \Jakmall\Recruitment\Calculator\Commands\AddCommand::class,
     \Jakmall\Recruitment\Calculator\Commands\SubtractCommand::class,
-    \Jakmall\Recruitment\Calculator\Commands\MultiplyCommand::class
+    \Jakmall\Recruitment\Calculator\Commands\MultiplyCommand::class,
+    \Jakmall\Recruitment\Calculator\Commands\DivideCommand::class
 ];
